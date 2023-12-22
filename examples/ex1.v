b4519caca692e9e732e2e351962d85f7ca74f587
Jishnu Sen <jishnu1@gmail.com>
2023-12-21 22:37:27 -0800

    rm

--- examples/ex1.v
+++
@@ -1,6 +0,0 @@
-(* This example showcases some features of the early server *)
-Lemma addn0 n : n + 0 = n.
-Proof. now induction n. Qed.
-
-Lemma muln0 n : n * 0 = 0.
-Proof. now induction n. Qed.
--- examples/modα.v
+++
@@ -1,5 +0,0 @@
-(* This is just a test for unicode filenames. *)
-
-Definition a := 3.
-
-Print a.
